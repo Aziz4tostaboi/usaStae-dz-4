17f1d739bea73f9d9840151f44cd636d64f718d5
Aziz4tostaboi <abylovaziz@gmail.com>
2023-11-27 10:59:55 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Post from './components/Post';
+import styles from './App.module.css';
 
-function App() {
+const DATA = [
+  {
+    id:1,
+    title: " aziz",
+    body:"london"
+  },
+  {
+    id:2,
+    title: " belek",
+    body:"USA"
+  },
+  {
+    id:3,
+    title: "isi",
+    body:"ROSSIA"
+  },
+  {
+    id:4,
+    title: "aman",
+    body:"bishkek"
+  },
+  {
+    id:5,
+    title: "nurik",
+    body:"JApan"
+  }
+]
+const App = () => {
+  const [posts, setPosts] = useState(DATA);
+  const [titleInput, setTitleInput] = useState('');
+
+  const handleAddPost = () => {
+    const newPost = {
+      id: Math.random(),
+      title: titleInput,
+    };
+
+    setPosts([...posts, newPost]);
+    setTitleInput('');
+  };
+
+  const handleRemovePost = (postId) => {
+    const updatedPosts = posts.filter((post) => post.id !== postId);
+    setPosts(updatedPosts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.inputContainer}>
+          <input
+              type="text"
+              value={titleInput}
+              onChange={(e) => setTitleInput(e.target.value)}
+              placeholder="Enter title"
+          />
+          <button onClick={handleAddPost}>Add</button>
+        </div>
+        {posts.map((post) => (
+            <Post
+                id={post.id}
+                title={post.title}
+                body={post.body}
+                onRemove={handleRemovePost}
+            />
+        ))}
+      </div>
   );
-}
+};
 
 export default App;
